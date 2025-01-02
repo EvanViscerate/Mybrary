d6af26042875b0ad9d810894c2e79142dd3d351a
EvanViscerate <evanviscerate@gmail.com>
2025-01-02 18:40:28 +0800

    Initial Setup

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL
-    // , {useNewUrlParser: true} >> deprecated option?
+    , {useNewUrlParser: true}
+    // >> deprecated option?
 )
 const db = mongoose.connection
 db.on('error', error => console.error(error))
